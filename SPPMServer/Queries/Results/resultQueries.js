9fc2919403d6ed2c81bd287da3ebe87f90fe8f73
LosGlennos <martinhugosvensson@gmail.com>
2015-12-21 13:36:45 +0100

    Refactoring
    refactoring underway

--- SPPMServer/Queries/Results/resultQueries.js
+++ SPPMServer/Queries/Results/resultQueries.js
@@ -9,97 +9,25 @@ module.exports = {
 
     registerKnockoutPlacings: function (User, result, res) {
         var firstPlace = result.placing1;
-        var secondPlace = result.placing2;
+		var secondPlace = result.placing2;
 
-        User.findOne({ _id: firstPlace.id }, function (findWinnerError, winningUser) {
-            if (findWinnerError) {
-                console.log(findWinnerError);
-            } else {
-                winningUser.points = winningUser.points + 2;
+		User.find({}, null, { sort: { 'placing': 1 } }, function (err, users) {
+		    var count = 1;
+            users.forEach(function(user) {
+				if (user._id === firstPlace.id) {
+					user.points = user.points + 2;
+				} else if (user._id === secondPlace.id) {
+				    user.points = user.points + 1;
+				}
 
-                winningUser.save(function(winningUserErr) {
-                    if (winningUserErr) {
-                        console.log(winningUserErr);
-                    } else {
-                        User.findOne({ _id: secondPlace.id }, function(findRunnerUpError, runnerUpUser) {
-                            if (findRunnerUpError) {
-                                console.log(findRunnerUpError);
-                            } else {
-                                runnerUpUser.points = runnerUpUser.points + 1;
-                                runnerUpUser.save(function(runnerUpErr) {
-                                    if (runnerUpErr) {
-                                        console.log(runnerUpErr);
-                                    } else {
-                                        User.find({}, null, { sort: { 'placing': 1 } }, function (err, users) {
-											if (err) {
-											    console.log(err);
-											} else {
-											    var count = 0;
-												users.forEach(function (user) {
-												    count++;
-											        if (user.points >= 5 && user.placing !== 1) {
-											            User.findOne({ placing: user.placing - 1 }, function(err, userToBeMoved) {
-											                if (err) {
-											                    console.log(err);
-											                } else {
-											                    userToBeMoved.placing = userToBeMoved.placing + 1;
-											                    userToBeMoved.save(function(err) {
-											                        if (err) {
-											                            console.log(err);
-											                        } else {
-											                            user.placing = user.placing - 1;
-											                            user.points = user.points - 5;
-											                            user.save(function(err) {
-											                                if (err) {
-											                                    console.log(err);
-											                                } else if (count === users.length) {
-											                                    User.findOne({ points: { $gt: 4 } }, function(error, theUser) {
-											                                        if (error) {
-											                                            console.log(error);
-											                                        } else if (theUser !== null && theUser.placing !== 1) {
-											                                            var userPoints = theUser.points;
-											                                            var numberOfMoves = Math.floor(userPoints / 5);
-											                                            for (var i = 0; i < numberOfMoves; i++) {
-											                                                User.findOne({ placing: theUser.placing - 1 }, function(err, theUserToBeMoved) {
-											                                                    if (err) {
-											                                                        console.log(err);
-											                                                    } else {
-											                                                        theUserToBeMoved.placing = theUserToBeMoved.placing + 1;
-											                                                        theUserToBeMoved.save(function(err) {
-											                                                            if (err) {
-											                                                                console.log(err);
-											                                                            } else {
-											                                                                theUser.placing = theUser.placing - 1;
-											                                                                theUser.points = theUser.points - 5;
-											                                                                theUser.save(function(err) {
-											                                                                    if (err) {
-											                                                                        console.log(err);
-											                                                                    }
-											                                                                });
-											                                                            }
-											                                                        });
-											                                                    }
-											                                                });
-											                                            }
-											                                        }
-											                                    });
-											                                }
-											                            });
-											                        }
-											                    });
-											                }
-											            });
-											        }
-											    });
-											}
-                                        });
-                                    }
-                                });
-                            }
-                        });
-                    }
-                });
-            }
-        });
+				if (user.placing !== 1 && user.placing >= 5) {
+					users[user.placing - 2].placing = users[user.placing - 2].placing + 1;
+					user.placing = user.placing - 1;
+				    user.points = user.points - 5;
+				}
+
+				count++;
+            });
+		});
     }
 }
